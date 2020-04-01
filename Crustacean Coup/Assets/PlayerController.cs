e48347dfdfa655c0325e0b3259cf64e8276a1b1e
Kevin Xu <kevinxu8099@gmail.com>
2020-03-31 20:55:48 -0700

    Animated Movements

--- Crustacean Coup/Assets/PlayerController.cs
+++ Crustacean Coup/Assets/PlayerController.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
-
+    public Animator am;
     private void Update()
     {
         
         if (Input.GetKey(KeyCode.A))
         {
             rb.velocity = new Vector2(-3, rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
+            am.SetBool("rolling",true);
+           
         }
 
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             rb.velocity = new Vector2(3, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
+            am.SetBool("rolling", true);
+
+        }
+        else
+        {
+            am.SetBool("rolling", false);
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector2(rb.velocity.x, 10f);
+            rb.velocity = new Vector2(rb.velocity.x, 5f);
         }
     }
 }
