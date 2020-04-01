21004919936be5915eaecba00adc28169d23c9b8
Kevin Xu <kevinxu8099@gmail.com>
2020-03-31 17:43:02 -0700

    Basic Platforming Movement

---
+++ Crustacean Coup/Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    private void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-3, rb.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(3, rb.velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 10f);
+        }
+    }
+}
